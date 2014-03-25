52918c9b223c6688e858372a28976e1d517a7b47
yingzhu.zyz <yingzhu.zyz@alibaba-inc.com>
2014-03-25 10:20:17 +0800

    ko

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-module.exports = require( './lib/modelproxy.js' );
+var InterfaceManager = require( './lib/interfacemanager' )
+  , ProxyFactory = require( './lib/proxyfactory' )
+  , ModelProxy = require( './lib/modelproxy' );
+
+var ifManager = new InterfaceManager( './interface.json' );
+
+ProxyFactory.use( ifManager );
+
+ModelProxy.use( ProxyFactory );
+
+module.exports = ModelProxy;
+
+/*
+var m = new ModelProxy('Search.*');
+m.getItems({q: 'jackjones'})
+.getItems({q: '冲锋衣'})
+.getItems({q: 'iphone'})
+.getItems({q: '春装'})
+.getItems({q: '连衣裙'})
+.getItems({q: '婚纱'})
+.getItems({q: 'jackjones'})
+.getItems({q: 'jackjones'})
+ .done(function(data) {
+    console.log( arguments );
+    this.getItems({q: '冲锋衣'})
+     .done( function(data1) {
+        console.log( 'hello' );
+        console.log(data1);
+     } ).done( function(data) {
+        console.log( data, 'nothing');
+     });
+
+ } );
+*/
+
+var m = new ModelProxy( 'Cart.getCart' );
+m.getCart({q:'kkk'})
+ .done( function( data ) {
+    console.log( data );
+ } );
+
